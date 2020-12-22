de6eb1a6a53cb74494d7765d97b8db57f58588e6
lavanya05-98 <76481292+lavanya05-98@users.noreply.github.com>
2020-12-22 18:55:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './contents/Home';
+import About from './contents/About';
+import Education from './contents/Education';
+import Experience from './contents/Experience';
+import Skills from './contents/Skills';
+import Contact from './contents/Contact';
+function App() {
+return (
+<Router>
+<div className="App">
+<Navbar />
+<Route exact path="/">
+<Home />
+</Route>
+<Route exact path="/about">
+<About />
+</Route>
+<Route exact path="/education">
+<Education />
+</Route>
+<Route exact path="/experience">
+<Experience />
+</Route>
+<Route exact path="/skills">
+<Skills />
+</Route>
+<Route exact path="/contact">
+<Contact />
+</Route>
+</div>
+</Router>
+)
+}
+export default App;
